faae1e7f4bd500a3fa9eb68aabf345cfebacaf71
Adan <adan@synthesis.co.za>
2020-04-13 10:25:28 +0200

    Initial Commit

---
+++ codebuild.tf
@@ -0,0 +1,30 @@
+resource "aws_codebuild_project" "codebuild_project" {
+    name         = "codebuild-${var.project_name}"
+    description  = "${var.project_description}"
+    service_role = "${aws_iam_role.codebuild_role.arn}"
+
+    artifacts {
+        type                 = "CODEPIPELINE"
+        artifact_identifier  = "build.log"
+    }
+
+    environment {
+        compute_type    = "BUILD_GENERAL1_SMALL"
+        type            = "LINUX_CONTAINER"
+        image           = "aws/codebuild/standard:2.0"
+        privileged_mode = true
+
+        dynamic "environment_variable" {
+            for_each = "${var.environment_vars}"
+            
+            content {
+              name  = "${environment_variable.key}"
+              value = "${environment_variable.value}"
+            }
+        }
+    }
+    source {
+        type     = "CODEPIPELINE"
+        location = "${data.aws_codecommit_repository.source_repository.repository_name}"
+    }
+}
---
+++ test/test.tf
@@ -0,0 +1,19 @@
+provider "aws" {
+    region  = "eu-west-1"
+    profile = "synthesis-internal-dev"
+}
+
+module "codepipline" {
+    source = "../"
+
+    project_name         = "wordpress-ami-pipeline"
+    project_description  = "wordpress AMI pipeline builder using the AMI builder repository"
+    artefact_bucket_name = "wordpress-ami-builder-artifact-bucket"
+    source_repository    = "ami-builder"
+
+    environment_vars = {
+      PACKER_SCRIPT  = "packer-wordpress"
+      VPC_ID         = "vpc-04f99e5833c3a909b"
+      SUBNET_ID      = "subnet-0e3c6194859275f16"
+    }
+}
